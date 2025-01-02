56144bc4352ec1416734b4f852328ece8f405514
Hitotsume-nozo <hitotsume.nozo@gmail.com>
2025-01-02 22:30:21 +0530

    Added the solution to question also unlocked all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(value,num) {
+    temp =''
+    for (i=0;i<num;i++){
+        temp += value;
+    }
+    if (num<0) {return "ERROR"};
+    return temp;
 };
 
 // Do not edit below this line
